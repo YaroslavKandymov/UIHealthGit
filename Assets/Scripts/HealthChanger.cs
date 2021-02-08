158366f028b91846ac4949b838ee1bd03850f532
Kandyimov Yaroslav <yakandyimov@gmail.com>
2021-02-09 00:06:41 +0300

    GitInit

---
+++ Assets/Scripts/HealthChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HealthChanger : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+
+    public void Heal()
+    {
+        _slider.DOValue(_slider.value + 10, 0.5f).SetEase(Ease.Linear);
+    }
+
+    public void Damage()
+    {
+        _slider.DOValue(_slider.value - 10, 0.5f).SetEase(Ease.Linear);
+    }
+}
